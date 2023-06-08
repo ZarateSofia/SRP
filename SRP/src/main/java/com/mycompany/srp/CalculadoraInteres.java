0304519b0184f526ea982cf8d743cbb433105a36
ZarateSofia <118627198+ZarateSofia@users.noreply.github.com>
2023-06-08 16:27:13 -0500

    Agregando archivo

---
+++ SRP/src/main/java/com/mycompany/srp/CalculadoraInteres.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.srp;
+
+/**
+ *
+ * @author CltControl
+ */
+public class CalculadoraInteres {
+    
+    public double CalculateIntereset(CuentaBancaria cuenta){
+        return 0.0;
+    }
+}
